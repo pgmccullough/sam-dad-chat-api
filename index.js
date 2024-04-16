290f82dc2facbcf953abdf074bbe80b37b17e79e
Patrick Glendon McCullough <p@mccullo.ug>
2024-04-16 00:25:02 -0400

    done for night

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
-    methods: ['GET', 'POST'],
-    credentials: true
+    methods: ['GET', 'POST']
   },
 });
 
