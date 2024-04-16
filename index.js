1ebca982b1cc534df576b73b6c9db3ae5c989ae6
Patrick Glendon McCullough <p@mccullo.ug>
2024-04-15 23:38:45 -0400

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Server } from 'socket.io';
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+const convo = [];
+const users = {};
+
+io.on('connection', (socket) => {
+  if(!users[socket.id]) {
+    socket.emit('message', convo);
+    socket.on('message', (data) => {
+      convo.push({...data, id: uuidv4(), date: Date.now()});
+      io.emit('message', convo);
+    })
+    socket.on('socketId', (data) => {
+      users[socket.id] = data;
+      io.emit('socketId', users);
+    });
+  }
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('socketId', users);
+  });
+});
+
+server.listen(8080, () => 'Server is running on port 8080');
