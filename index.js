7c7fc0178f2e03e598216ba53fec27fb21816a96
Patrick Glendon McCullough <p@mccullo.ug>
2024-04-15 23:46:01 -0400

    port fix for deploy

--- index.js
+++ index.js
@@ -39,4 +39,4 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(8080, () => 'Server is running on port 8080');
+server.listen(process.env.PORT || 8080, () => 'Server is running on port 8080');
